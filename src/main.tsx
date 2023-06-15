dd2eb68558ef6fc82faff0f30120939b8916338a
Christopher San Diego <christopher.sandiego@2359media.com>
2023-06-15 16:34:32 +0800

    Try to adapt a React component to a Web Component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+// import { Icon } from "@molb/gobiz-styleguide"
+// import "@molb/gobiz-styleguide/src/styles/root.scss"
+import { createCustomElement, DOMModel, byContentVal, byAttrVal, registerEvent } from "@adobe/react-webcomponent"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const Hello = () => <h1>Hello</h1>
+
+const HelloWC = createCustomElement(Hello, DOMModel, "container");
+console.log(HelloWC);
+customElements.define('x-hello', HelloWC)
